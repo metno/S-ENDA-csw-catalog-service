66e9b568ea350a4a7d44bfd37ce17a7f606fb278
Morten W. Hansen <morten.wergeland.hansen@gmail.com>
2020-08-27 15:32:18 +0200

    Overwrite the default ENTRYPOINT of the pycsw image

--- build_container.sh
+++ build_container.sh
@@ -1,3 +1,9 @@
+cd /vagrant
+mkdir lib
+cd lib
+git clone git@github.com:metno/mmd.git
+
+cd /vagrant
 name=catalog-dev
 docker build -t $name -f Dockerfile.devel .
 
@@ -6,5 +12,6 @@ docker rm $name 2> /dev/null
 
 # build container
 docker create -it --name=$name \
+    --entrypoint "" \
     -v /vagrant/lib/mmd/mmd_utils:/usr/lib/python3.8/site-packages/mmd_utils \
-    $name
+    $name sh
