0062091e3b9652f4b54b758aa30ac5b232e9149e
Morten W. Hansen <morten.wergeland.hansen@gmail.com>
2020-09-10 15:58:04 +0200

    #53: added postgis config

--- catalog-service-api/pycsw_setup.sh
+++ catalog-service-api/pycsw_setup.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+if [ -z "$PYCSW_DATABASE" ]; then
+  PYCSW_DATABASE="sqlite:////home/pycsw/tests.db"
+fi
+
+echo database="$PYCSW_DATABASE" >> /etc/pycsw/pycsw.cfg
+#database=postgresql://csw_user:csw_password@postgres:5432/csw_db
+
 python3 /usr/bin/pycsw-admin.py -c setup_db -f /etc/pycsw/pycsw.cfg
 python3 /usr/bin/pycsw-admin.py -c load_records -f /etc/pycsw/pycsw.cfg -p "$ISO_STORE" -r -y
 
