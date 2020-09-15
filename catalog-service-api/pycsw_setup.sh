037daa25aeb9fc80ff4c4c385288a6b30ec6637e
Arnulf Heimsbakk <aheimsbakk@met.no>
2020-09-15 16:29:11 +0200

    remove database connection if already defined in file, ie restart of container

--- catalog-service-api/pycsw_setup.sh
+++ catalog-service-api/pycsw_setup.sh
@@ -4,6 +4,7 @@ if [ -z "$PYCSW_DATABASE" ]; then
   PYCSW_DATABASE="sqlite:////home/pycsw/tests.db"
 fi
 
+sed -i '/^database=/d' /etc/pycsw/pycsw.cfg
 echo database="$PYCSW_DATABASE" >> /etc/pycsw/pycsw.cfg
 #database=postgresql://csw_user:csw_password@postgres:5432/csw_db
 
