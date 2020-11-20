cb3b8aba63a30dcb4f357dfb45aa5c1b871633b8
Morten W. Hansen <morten.wergeland.hansen@gmail.com>
2020-11-20 19:19:06 +0100

    #10: this does not work on vagrant up, but ssh into the vm and running deploy-metadata.sh from inside the vm works

--- deploy-metadata.sh
+++ deploy-metadata.sh
@@ -26,12 +26,12 @@ docker-compose run --rm \
     xmlconverter.py -i /mmddir -o /isostore -t /usr/local/share/xslt/mmd-to-iso.xsl
 
 ## Restart catalog-service-api
-#docker-compose rm -sf catalog-service-api
-#docker-compose up -d catalog-service-api
+docker-compose rm -sf catalog-service-api
+docker-compose up -d catalog-service-api
 
 # This would be preferred instead of rebuilding but I can't make it work
 # Ingest metadata from ISO19139 xml files
-docker exec vagrant_catalog-service-api_1 python3 /usr/bin/pycsw-admin.py -c load_records -f /etc/pycsw/pycsw.cfg -p "$ISO_STORE" -r -y
+#docker exec vagrant_catalog-service-api_1 python3 /usr/bin/pycsw-admin.py -c load_records -f /etc/pycsw/pycsw.cfg -p "$ISO_STORE" -r -y
 
 # Clean up
 #rm /vagrant/lib/isostore/*
