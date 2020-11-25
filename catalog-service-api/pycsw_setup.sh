2877266ec2f5955a573d259b6e567f8d2120e961
Morten W. Hansen <morten.wergeland.hansen@gmail.com>
2020-11-25 12:03:52 +0100

    #14: the catalog-service-api should only run the service, not index any metadata

--- catalog-service-api/pycsw_setup.sh
+++ catalog-service-api/pycsw_setup.sh
@@ -10,9 +10,10 @@ echo database="$PYCSW_DATABASE" >> /etc/pycsw/pycsw.cfg
 
 python3 /usr/bin/pycsw-admin.py -c setup_db -f /etc/pycsw/pycsw.cfg
 
-echo ""
-echo "Indexing records from " $ISO_STORE
-echo ""
-python3 /usr/bin/pycsw-admin.py -c load_records -f /etc/pycsw/pycsw.cfg -p "$ISO_STORE" -r -y
+# We can do the indexing in other places
+#echo ""
+#echo "Indexing records from " $ISO_STORE
+#echo ""
+#python3 /usr/bin/pycsw-admin.py -c load_records -f /etc/pycsw/pycsw.cfg -p "$ISO_STORE" -r -y
 
 python3 /usr/local/bin/entrypoint.py
